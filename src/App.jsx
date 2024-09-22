01a3de2df57782dec37258d3e65985c014c1f86b
WilliamFernsV3 <williamfernsv3@gmail.com>
2024-09-22 10:34:04 +0200

    fix(App): small syntax fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import { Provider as ReduxProvider, connect } from "react-redux";
 import { legacy_createStore as createStore } from "redux";
 import PropTypes from "prop-types";
 
-
-
-
 class Presentational extends React.Component {
   constructor(props) {
     super(props)
